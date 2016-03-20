a0a070f88dbcefa296d699e870ddfe008fdbc97d
Amr Alaa <n1amr1@gmail.com>
2016-03-20 12:02:35 +0200

    change name of the form

--- ProcessScheduler/MainForm.cs
+++ ProcessScheduler/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ProcessScheduler
 {
-  public partial class Form1 : Form
+  public partial class Scheduler : Form
   {
-    public Form1()
+    public Scheduler()
     {
       InitializeComponent();
     }
